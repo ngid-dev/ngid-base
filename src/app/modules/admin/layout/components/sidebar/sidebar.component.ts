a040ea022b793363c299f2d1619430a63d4b5f88
danamasdevtisufa <titus.sutio@danamas.co.id>
2023-02-23 20:30:25 +0700

    add: Angular Base - Membuat Tampilan Sidebar Layout Admin Part 1

--- src/app/modules/admin/layout/components/sidebar/sidebar.component.ts
+++ src/app/modules/admin/layout/components/sidebar/sidebar.component.ts
@@ -4,13 +4,62 @@ import { BaseComponent } from 'src/app/core/base';
 @Component({
   selector: 'admin-layout-sidebar',
   templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.scss'],
 })
 export class SidebarComponent extends BaseComponent {
+  public menus: Array<{ name: string; routerLink: string }>;
+
   constructor() {
     super('module.admin.layout.component.sidebar');
   }
 
   protected onInit(): void {
+    this.setStateMenus();
     this.setStateReady();
+  }
+
+  private setStateMenus(): void {
+    this.menus = [
+      {
+        name: 'Dashboard',
+        routerLink: '/',
+      },
+      {
+        name: 'Posts',
+        routerLink: '/posts',
+      },
+      {
+        name: 'Media',
+        routerLink: '/media',
+      },
+      {
+        name: 'Pages',
+        routerLink: '/pages',
+      },
+      {
+        name: 'Comments',
+        routerLink: '/comments',
+      },
+      {
+        name: 'Appearance',
+        routerLink: '/appearance',
+      },
+      {
+        name: 'Plugins',
+        routerLink: '/plugins',
+      },
+      {
+        name: 'Users',
+        routerLink: '/users',
+      },
+      {
+        name: 'Tools',
+        routerLink: '/tools',
+      },
+      {
+        name: 'Settings',
+        routerLink: '/settings',
+      },
+    ];
   }
 }
